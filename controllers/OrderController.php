d4ca434fbe0d528c3b583d8b8a53eacd43829db7
samnew <samnew@126.com>
2016-11-02 00:10:16 +0800

    add layout2 and modify view template

--- controllers/OrderController.php
+++ controllers/OrderController.php
@@ -3,11 +3,13 @@ namespace app\controllers;
 use yii\web\controller;
 
 class OrderController extends Controller{
-	public $layout = 'layout1';
+	
 	public function actionIndex(){
+		$this->layout = 'layout2';
 		return $this->render('index.php');
 	}
 	public function actionCheck(){
+		$this->layout = 'layout1';
 		return $this->render('check.php');
 	}
 }
--- controllers/ProductController.php
+++ controllers/ProductController.php
@@ -4,13 +4,13 @@ namespace app\controllers;
 use yii\web\controller;
 
 class ProductController extends Controller{
-	public $layout = false;
 	public function actionIndex(){
-	
+		$this->layout = 'layout2';
 		return $this->render('index.php');
 	}
 
 	public function actionDetail(){
+		$this->layout = 'layout2';
 		return $this->render('detail.php');
 	}
 } 
